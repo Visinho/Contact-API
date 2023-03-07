6e2fd436ccb3f8b43990cca75b42b36655cc8f18
Visinho <Elvisvisinho@rocketmail.com>
2023-03-07 18:48:38 +0100

    Created the user model

---
+++ models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please add the username"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please add the email address"],
+    },
+    phone: {
+        type: String,
+        required: [true, "Please add the contact phone number"],
+        unique: [true, "Email address already taken"],
+    }, 
+    password: {
+        type: String,
+        required: [true, "Please add the password"],
+    }, 
+},{
+    timestamps: true,
+});
+
+
+module.exports = mongoose.model("User", userSchema);
